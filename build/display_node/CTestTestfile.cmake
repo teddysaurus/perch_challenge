996c641773b18919736c6b5689058fbbda1aa393
Simon Jensen-Fellows <sjf@myfanwy.com>
2021-06-06 18:42:37 -0400

    clean up

--- build/display_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sjf/source/perch_ws/src/display_node
-# Build directory: /home/sjf/source/perch_ws/build/display_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
